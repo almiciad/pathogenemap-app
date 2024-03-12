a7a09757417f222cbb9472732f0ba9ce24b0acec
almiciad <89491197+almiciad@users.noreply.github.com>
2024-03-12 17:23:58 +0000

    updating gene picker

--- pages/1_App_Demo.py
+++ pages/1_App_Demo.py
@@ -14,13 +14,32 @@ st.write(
     """
 )
 
-genes_data = pd.read_csv("https://raw.githubusercontent.com/wesleywchang/2024-capstone-gene-nlp/main/fine_tuning_dataset/fine_tuning_dataset_v1.2/finetuning_training_data/processed/in_vivo_gene_perturbations_test_sample1k_random.csv?token=GHSAT0AAAAAACMV4AR5P7IUZIB5K4RVCZEWZPQSYLQ")
+def data_frame_demo():
+    @st.cache_data
+    def get_UN_data():
+        DATA_URL = "https://raw.githubusercontent.com/wesleywchang/2024-capstone-gene-nlp/main/fine_tuning_dataset/fine_tuning_dataset_v1.2/finetuning_training_data/processed/in_vivo_gene_perturbations_test_sample1k_random.csv?token=GHSAT0AAAAAACMV4AR5P7IUZIB5K4RVCZEWZPQSYLQ"
+        df = pd.read_csv(DATA_URL)
+        return df.set_index("label")
 
-bygenes = genes_data.set_index(['label'])
-gp = st.selectbox("Choose label", list(bygenes.index))
-st.write("You selected:", gp)
-gpdata = bygenes.loc[gp]
-st.write(bygenes.sort_index())
+    try:
+        df = get_UN_data()
+        countries = st.multiselect(
+            "Choose perbutation", list(df.index)
+        )
+        if not countries:
+            st.error("Please select at least one country.")
+        else:
+            data = df.loc[countries]
+            #data /= 1000000.0
+            st.write("### Gene Information", data.sort_index())
+    except URLError as e:
+        st.error(
+            """
+            **This demo requires internet access.**
+            Connection error: %s
+        """
+            % e.reason
+        )
 
-
+data_frame_demo()
 
